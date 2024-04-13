e42857bec01c24623197f9ef2f3eb26ad9101a73
DangVanCong2301 <dangvancong2301@gmail.com>
2024-04-13 15:48:36 +0700

    add songs

--- assets/js/list-music.js
+++ assets/js/list-music.js
@@ -9,7 +9,7 @@ let allSongs = [
     {
         id:"2",
         songName:"Enchated", 
-        artist: "Taylor",
+        artist: "Taylor Swift",
         poster:"enchanted",
         src: "enchanted"
     },
@@ -69,5 +69,33 @@ let allSongs = [
         poster:"its_okay_if_you_forget_me",
         src: "its_okey_if_you_forget_me"
     },
+    {
+        id:"11",
+        songName:"Let Me Go", 
+        artist: "Hailee Steinfeld, Alesso",
+        poster:"let_me_go",
+        src: "let_me_go"
+    },
+    {
+        id:"12",
+        songName:"Closer", 
+        artist: "Halsey",
+        poster:"closer",
+        src: "closer"
+    },
+    {
+        id:"13",
+        songName:"You Broke Me First", 
+        artist: "Tate McRae",
+        poster:"you_broke_me_first",
+        src: "you_broke_me_first"
+    },
+    {
+        id:"14",
+        songName:"Let Me Love You", 
+        artist: "Justin Bieber",
+        poster:"let_me_love_you",
+        src: "let_me_love_you"
+    }
 
 ]
